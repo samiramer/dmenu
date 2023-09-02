eee865ed178cdba1cae3a01ba5efc0a25710cbf6
samer <samirmamer@gmail.com>
2023-09-01 22:21:43 -0400

    Add gruvbox colors

--- config.def.h
+++ config.def.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"CaskaydiaCove Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d8a657", "#1d2021" },
+	[SchemeSel] = { "#1d2021", "#d8a657" },
+	[SchemeSelHighlight] = { "#d8a657", "#1d2021" },
+	[SchemeNormHighlight] = { "#d8a657", "#1d2021" },
+	[SchemeOut] = { "#1d2021", "#d8a657" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
