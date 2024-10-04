9c4ba902bb704f8c493852ceb2a4529eaa87df88
Samir Amer <samir@sunnybyte.com>
2024-10-05 02:09:48 +0400

    Switch to gruvbox

--- config.def.h
+++ config.def.h
@@ -10,16 +10,16 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	//[SchemeNorm] = { "#d8a657", "#1d2021" },
-	//[SchemeSel] = { "#1d2021", "#d8a657" },
-	//[SchemeSelHighlight] = { "#d8a657", "#1d2021" },
-	//[SchemeNormHighlight] = { "#d8a657", "#1d2021" },
-	//[SchemeOut] = { "#1d2021", "#d8a657" },
-	[SchemeNorm] = { "#dedede", "#011627" },
-	[SchemeSel] = { "#011627", "#999999" },
-	[SchemeSelHighlight] = { "#dedede", "#011627" },
-	[SchemeNormHighlight] = { "#dedede", "#011627" },
-	[SchemeOut] = { "#011627", "#dedede" },
+	[SchemeNorm] = { "#d8a657", "#1d2021" },
+	[SchemeSel] = { "#1d2021", "#d8a657" },
+	[SchemeSelHighlight] = { "#d8a657", "#1d2021" },
+	[SchemeNormHighlight] = { "#d8a657", "#1d2021" },
+	[SchemeOut] = { "#1d2021", "#d8a657" },
+	// [SchemeNorm] = { "#dedede", "#011627" },
+	// [SchemeSel] = { "#011627", "#999999" },
+	// [SchemeSelHighlight] = { "#dedede", "#011627" },
+	// [SchemeNormHighlight] = { "#dedede", "#011627" },
+	// [SchemeOut] = { "#011627", "#dedede" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
